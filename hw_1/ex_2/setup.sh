ac12b0fb208e34810f6a7dc54cbb5b251035dfe2
PSeasword <148008778+PSeasword@users.noreply.github.com>
2023-10-30 11:14:23 +0100

    Removed /content in setup.sh

--- hw_1/ex_2/setup.sh
+++ hw_1/ex_2/setup.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/NVIDIA/cuda-samples.git
 
 cp -rf cuda-samples/Samples/1_Utilities/deviceQuery ./deviceQuery
-cp -rf /content/cuda-samples/Samples/1_Utilities/bandwidthTest ./bandwidthTest
+cp -rf cuda-samples/Samples/1_Utilities/bandwidthTest ./bandwidthTest
 
 nvcc -Icuda-samples/Common deviceQuery/deviceQuery.cpp -o deviceQuery/deviceQuery.out
 nvcc -Icuda-samples/Common bandwidthTest/bandwidthTest.cu -o bandwidthTest/bandwidthTest.out
