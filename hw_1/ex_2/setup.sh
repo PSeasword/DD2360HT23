45f27b2ddab4049c5716e7d2c318d228744337e3
PSeasword <148008778+PSeasword@users.noreply.github.com>
2023-10-30 10:58:37 +0100

    Create setup.sh

---
+++ hw_1/ex_2/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/NVIDIA/cuda-samples.git
+
+cp -rf cuda-samples/Samples/1_Utilities/deviceQuery ./deviceQuery
+cp -rf /content/cuda-samples/Samples/1_Utilities/bandwidthTest ./bandwidthTest
+
+nvcc -Icuda-samples/Common deviceQuery/deviceQuery.cpp -o deviceQuery/deviceQuery.out
+nvcc -Icuda-samples/Common bandwidthTest/bandwidthTest.cu -o bandwidthTest/bandwidthTest
