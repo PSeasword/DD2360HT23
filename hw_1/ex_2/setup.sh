b80907c22cba06dbece103e712cd3a6ed419e6e4
PSeasword <148008778+PSeasword@users.noreply.github.com>
2023-10-30 11:08:26 +0100

    Added chmod +x for other scripts in setup.sh

--- hw_1/ex_2/setup.sh
+++ hw_1/ex_2/setup.sh
@@ -7,3 +7,6 @@ cp -rf /content/cuda-samples/Samples/1_Utilities/bandwidthTest ./bandwidthTest
 
 nvcc -Icuda-samples/Common deviceQuery/deviceQuery.cpp -o deviceQuery/deviceQuery.out
 nvcc -Icuda-samples/Common bandwidthTest/bandwidthTest.cu -o bandwidthTest/bandwidthTest.out
+
+chmod +x deviceQuery.sh
+chmod +x bandwidthTest.sh
