a16aa3d0840c38e3b8eb9703f5eadcf9ea608f32
PSeasword <148008778+PSeasword@users.noreply.github.com>
2023-10-30 13:06:34 +0100

    Create furtherMatrixSizes.sh

---
+++ hw_2/ex_2/furtherMatrixSizes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+make clean
+make
+
+inputDims1=(550 600 650 700 750 800 850)
+inputDim2=1024
+inputDim3=4150
+
+> output_matrix_sizes.txt
+echo "Writing outputs to output_matrix_sizes.txt"
+
+for inputDim1 in "${inputDims1[@]}"; do
+  echo "Currently doing $inputDim1 $inputDim2 $inputDim3"
+  ./hw2_ex2.out "$inputDim1" "$inputDim2" "$inputDim3" >> output_matrix_sizes.txt
+done
+
+cat output_matrix_sizes.txt
