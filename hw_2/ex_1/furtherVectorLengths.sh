f2bec29dc5288d0df46e6062fa40ed8a58e53a03
PSeasword <148008778+PSeasword@users.noreply.github.com>
2023-10-30 12:54:41 +0100

    Create furtherVectorLengths.sh

---
+++ hw_2/ex_1/furtherVectorLengths.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+make clean
+make
+
+inputLengths=(200000 400000 600000 800000 1000000 1200000 1400000 1600000 1800000 2000000)
+
+> output_vector_lengths.txt
+echo "Writing outputs to output_vector_lengths.txt"
+
+for inputLength in "${inputLengths[@]}"; do
+  ./hw2_ex1.out "$inputLength" >> output_vector_lengths.txt
+done
+
+cat output_vector_lengths.txt
